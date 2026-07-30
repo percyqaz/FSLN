41599ac7a4e2fc87ebcdfd82322235fa708c7216
Percy Qaz <percyqaz@yahoo.co.uk>
2026-07-30 21:21:40 +0100

    fix: selection resetting wrong on autoreload

--- State/Modes/NormalMode.fs
+++ State/Modes/NormalMode.fs
@@ -8,7 +8,7 @@ type NormalMode =
 
     member this.Reload(workspace: Workspace) : NormalMode =
         let solution = workspace.ReloadSolution()
-        { Solution = solution; Selected = Selection.Solution(this.Solution) } // todo: recover selection
+        { Solution = solution; Selected = Selection.Find(this.Selected, solution) }
 
     member this.AutoReload(workspace: Workspace) : NormalMode =
         if this.Solution.HasExternalChange() then this.Reload(workspace) else this
--- State/Modes/Selection.fs
+++ State/Modes/Selection.fs
@@ -28,6 +28,29 @@ type Selection =
         | Project project -> Some(Parent.Project(project))
         | Solution _ -> None
 
+    static member Find(selection: Selection, tree: Solution) : Selection =
+
+        let inline search_matching_folder (folder: FileTreeFolder) : FileTreeFolder option =
+            tree.Projects |> Seq.collect _.EnumerateSubfolders() |> Seq.tryFind(fun f -> f.FullPath = folder.FullPath)
+
+        let inline search_matching_file (file: FileTreeFile) : FileTreeFile option =
+            tree.Projects |> Seq.collect _.EnumerateFiles() |> Seq.tryFind(fun f -> f.FullPath = file.FullPath)
+
+        match selection with
+        | Selection.Solution _ -> Selection.Solution(tree)
+        | Selection.Project project ->
+            match tree.Projects |> Seq.tryFind(fun p -> p.FullPath = project.FullPath) with
+            | Some p -> Selection.Project(p)
+            | None -> Selection.Solution(tree)
+        | Selection.Folder folder ->
+            match search_matching_folder(folder) with
+            | Some f -> Selection.Folder(f)
+            | None -> Selection.Solution(tree)
+        | Selection.File file ->
+            match search_matching_file(file) with
+            | Some f -> Selection.File(f)
+            | None -> Selection.Solution(tree)
+
     member this.Equals(file: FileTreeFile) : bool = this = File file
     member this.Equals(folder: FileTreeFolder) : bool = this = Folder folder
     member this.Equals(project: Project) : bool = this = Project project
